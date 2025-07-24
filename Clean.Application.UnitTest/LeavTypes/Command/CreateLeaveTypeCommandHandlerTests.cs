acc43017433263e88c614faa75b1432bfeac58ef
Masoud Kian <masoudkiannezhad@gmail.com>
2025-07-24 17:59:11 +0330

    Testing is done

---
+++ Clean.Application.UnitTest/LeavTypes/Command/CreateLeaveTypeCommandHandlerTests.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Clean.Application.Contracts.Persistence.EntitiesRepository;
+using Clean.Application.DTO.LeaveType;
+using Clean.Application.Features.LeaveTypes.Handlers.Command;
+using Clean.Application.Features.LeaveTypes.Requests.Command;
+using Clean.Application.Profiles;
+using Clean.Application.Responses;
+using Clean.Application.UnitTest.Mocks;
+using Moq;
+using Shouldly;
+
+namespace Clean.Application.UnitTest.LeavTypes.Command
+{
+    public class CreateLeaveTypeCommandHandlerTests
+    {
+        IMapper _mapper;
+        Mock<ILeaveTypeRepository> _mockRepository;
+        CreateLeaveTypeDto _leaveTypeDto;
+        public CreateLeaveTypeCommandHandlerTests()
+        {
+            _mockRepository = MockLeaveTypeRepository.GetLeaveTypeRepository();
+
+            var mapperConfig = new MapperConfiguration(m =>
+            {
+                m.AddProfile<MappingProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+
+            _leaveTypeDto = new CreateLeaveTypeDto()
+            {
+                DefaultDay = 15,
+                Name = "Test LeaveType DTO"
+            };
+        }
+
+        [Fact]
+        public async Task CreateLeaveType()
+        {
+            var handler = new CreateLeaveTypeCommandHandler(_mockRepository.Object,_mapper);
+
+            var result = await handler.Handle(new CreateLeaveTypeCommand()
+            { CreateLeaveTypeDto = _leaveTypeDto, }, CancellationToken.None);
+
+            result.ShouldBeOfType<BaseCommandResponse>();
+
+            var leaveType = await _mockRepository.Object.GetAllEntities();
+
+            leaveType.Count.ShouldBe(3);
+        }
+    }
+}
--- Clean.Application.UnitTest/LeavTypes/Queries/GetLeaveTypeListRequestHandlerTests.cs
+++ Clean.Application.UnitTest/LeavTypes/Queries/GetLeaveTypeListRequestHandlerTests.cs
@@ -1,7 +1,12 @@
 ﻿using AutoMapper;
 using Clean.Application.Contracts.Persistence.EntitiesRepository;
+using Clean.Application.DTO.LeaveType;
+using Clean.Application.Features.LeaveTypes.Handlers.Queries;
+using Clean.Application.Features.LeaveTypes.Requests.Queries;
+using Clean.Application.Profiles;
 using Clean.Application.UnitTest.Mocks;
 using Moq;
+using Shouldly;
 
 namespace Clean.Application.UnitTest.LeavTypes.Queries
 {
@@ -13,6 +18,24 @@ namespace Clean.Application.UnitTest.LeavTypes.Queries
         public GetLeaveTypeListRequestHandlerTests()
         {
             _mockRepository = MockLeaveTypeRepository.GetLeaveTypeRepository();
+
+            var mapperConfig = new MapperConfiguration(m =>
+            {
+                m.AddProfile<MappingProfile>();
+            });
+            _mapper = new Mapper(mapperConfig);
+        }
+
+        [Fact]
+        public async Task GetLeaveTypeListTest()
+        {
+            var handler = new GetLeaveTypeListHandlerQuery(_mockRepository.Object, _mapper);
+
+            var result = await handler.Handle(new GetLeaveTypeListRequestQuery(), CancellationToken.None);
+
+
+            result.ShouldBeOfType<List<LeaveTypeDTO>>();
+            result.Count.ShouldBe(2);   
 
 
         }
