09ddb829115d543db12325a28bb4259551547c7e
Masoud Kian <masoudkiannezhad@gmail.com>
2025-07-24 17:27:31 +0330

    set Xunit Test

---
+++ Clean.Application.UnitTest/LeavTypes/Queries/GetLeaveTypeListRequestHandlerTests.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Clean.Application.Contracts.Persistence.EntitiesRepository;
+using Clean.Application.UnitTest.Mocks;
+using Moq;
+
+namespace Clean.Application.UnitTest.LeavTypes.Queries
+{
+    public class GetLeaveTypeListRequestHandlerTests
+    {
+        IMapper _mapper;
+        Mock<ILeaveTypeRepository> _mockRepository;
+
+        public GetLeaveTypeListRequestHandlerTests()
+        {
+            _mockRepository = MockLeaveTypeRepository.GetLeaveTypeRepository();
+
+
+        }
+
+
+    }
+}
---
+++ Clean.Application.UnitTest/Mocks/MockLeaveTypeRepository.cs
@@ -0,0 +1,41 @@
+﻿using Clean.Application.Contracts.Persistence.EntitiesRepository;
+using Clean.Domain.Models;
+using Moq;
+
+namespace Clean.Application.UnitTest.Mocks
+{
+    public static class MockLeaveTypeRepository
+    {
+        public static Mock<ILeaveTypeRepository> GetLeaveTypeRepository()
+        {
+            var leaveTypes = new List<LeaveType>()
+            {
+                new LeaveType
+                {
+                Id = 1,
+                DefaultDay = 10,
+                Name = "Test Vacation"
+                },
+                new LeaveType
+                {
+                Id = 2,
+                DefaultDay = 15,
+                Name = "Test Sick"
+                }
+            };
+
+            var mockRepo = new Mock<ILeaveTypeRepository>();
+            mockRepo.Setup(r => r.GetAllEntities()).ReturnsAsync(leaveTypes);
+                                            // Controlling method input that does not have a different type
+            mockRepo.Setup(r => r.CreateAsync(It.IsAny<LeaveType>()))
+                .ReturnsAsync((LeaveType leavetype) =>
+                {
+                    leaveTypes.Add(leavetype);
+                    return leavetype;
+                });
+
+
+            return mockRepo;
+        }
+    }
+}
